bd97f9a4ce833bc267c6d16c18edae82048ef3e5
bmehder <bmehder@gmail.com>
2021-09-18 13:19:42 +0200

    added providers route

---
+++ src/routes/providers.svelte
@@ -0,0 +1,58 @@
+<script>
+  import Section from '$lib/Section.svelte'
+</script>
+
+<svelte:head>
+  <meta
+    name="description"
+    content="Your patients look to you as their contact for help across the board. Consider us as a resource that you can confidently recommend to deal with chronic wounds."
+  />
+  <title>Restore Medical Group | Providers | Golden, CO</title>
+
+  <script
+    async
+    src="https://www.googletagmanager.com/gtag/js?id=UA-185413763-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || []
+    function gtag() {
+      dataLayer.push(arguments)
+    }
+    gtag('js', new Date())
+
+    gtag('config', 'UA-185413763-1')
+  </script>
+</svelte:head>
+
+<Section boxed background="white" padding="4rem" title="Providers">
+  <p>
+    Whether you have a patient that needs a wound care referral or if you are a
+    current wound care provider who has a patient who has not responded to the
+    treatment you have provided so far, please consider us as a resource for
+    healing.
+  </p>
+
+  <p>
+    Your patients look to you as their contact for help across the board.
+    Consider us as a resource that you can confidently recommend to deal with
+    chronic wounds. We are not looking to replace the care you provide but to
+    augment your service for more stubborn situations. We will work under your
+    guidance if you would like to take the lead or you can turn the very
+    specific work we do over to us completely, knowing that we will keep you
+    informed every step along the way.
+  </p>
+
+  <p>
+    We always provide an evaluation up front to make sure the patient is a good
+    candidate for the Restore process. We also make sure insurance is
+    pre-authorized so there are no surprises. For those that qualify, we have
+    found that the process is not long and drawn out but provides healing at
+    around 21 days or within several months. Every patient is different of
+    course. Once the major part of our work is done your patient is yours again
+    for follow up and to continue under your care.
+  </p>
+
+  <p>
+    We are actively looking for partners to help us with patients who have
+    struggled in the past.
+  </p>
+</Section>
