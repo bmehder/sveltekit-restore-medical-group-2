7580c40dc2a2c428626a20394db456c7399d7868
bmehder <bmehder@gmail.com>
2021-09-18 13:11:57 +0200

    added layout file

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,27 @@
+<script context="module">
+  export const load = async ({ page }) => ({
+    props: {
+      key: page.path,
+    },
+  })
+</script>
+
+<script>
+  import '../app.css'
+  import PageTransition from '$lib/PageTransition.svelte'
+  import Head from '$lib/Head.svelte'
+  import Header from '$lib/Header.svelte'
+  import Bottom from '$lib/Bottom.svelte'
+  import Footer from '$lib/Footer.svelte'
+
+  export let key
+</script>
+
+<Head />
+
+<Header background="#111" />
+<PageTransition refresh={key}>
+  <slot />
+</PageTransition>
+<Bottom />
+<Footer background="#111" color="white">Restore Medical Group</Footer>
