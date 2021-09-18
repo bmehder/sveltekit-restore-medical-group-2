74b5451c699ab3df865b8000c5dcf133060ada72
bmehder <bmehder@gmail.com>
2021-09-18 13:15:43 +0200

    removed about added patients

---
+++ src/routes/patients.svelte
@@ -0,0 +1,57 @@
+<script>
+  import Section from '$lib/Section.svelte'
+</script>
+
+<svelte:head>
+  <meta
+    name="description"
+    content="There are many treatment options for wounds and Restore Medical Group will work with you to provide the best option for care."
+  />
+  <title>Restore Medical Group | Patients | Golden, CO</title>
+
+  <script
+    async
+    src="https://www.googletagmanager.com/gtag/js?id=UA-185413763-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || []
+    function gtag() {
+      dataLayer.push(arguments)
+    }
+    gtag('js', new Date())
+
+    gtag('config', 'UA-185413763-1')
+  </script>
+</svelte:head>
+
+<Section boxed background="white" padding="4rem" title="Patients">
+  <h4>Frustrated by a chronic wound?</h4>
+
+  <p>
+    Wounds can be created many ways; surgery, injury, Diabetes, pressure, even
+    blood clots can lead to surface wounds. Other factors can complicate
+    treatment from medication to radiation treatment or circulation problems.
+    Even what looks like a simple wound can become a serious (not to mention
+    painful!) issue.
+  </p>
+  <p>
+    There are many treatment options for wounds and Restore Medical Group will
+    work with you to provide the best option for care. Serious wounds can make
+    it difficult to get around and so we offer multiple clinics along the Front
+    Range as well as the option of traveling to the facility where you are or
+    even your home.
+  </p>
+  <p>
+    We specialize in treating chronic wounds with the Restore process that could
+    include the use of amnio products. These products use your own tissue to
+    create new skin and have proven very effective even on people who have been
+    treated with more conventional wound practices.
+  </p>
+  <p>
+    Don’t give up! This process has been approved by many insurance groups as
+    well as Medicare Part B. What was once cost-prohibitive is now within the
+    reach of many.
+  </p>
+  <p>
+    <a href="/contact">Contact us</a> to discuss your situation and set up an evaluation.
+  </p>
+</Section>
