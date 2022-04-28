4d8b479a21be698d02dcaaa7fa5f7888e9ceb1fd
bmehder <bmehder@gmail.com>
2022-04-28 16:56:17 +0200

    changes from Apr 28 email

--- src/lib/Bottom.svelte
+++ src/lib/Bottom.svelte
@@ -4,27 +4,27 @@
 </script>
 
 <Section padding="4rem 0 3rem" background="#333">
-  <h4>We offer multiple locations for in-clinic treatments.</h4>
+  <h4>Flexible, In-Home or In-Facility Services Available Throughout the Front Range.</h4>
   <div>
-    <p>
+    <!-- <p>
       <big><strong>Golden, CO</strong></big><br />
       400 Indian Street, Suite 280<br />
       Golden, CO 80401
-    </p>
+    </p> -->
 
     <p>
-      <big><strong>Thornton, CO</strong></big><br />
-      9195 Grant Street, Suite 305<br />
-      Thornton, CO 80229
+      <big><strong>Colorado Central Office</strong></big><br />
+      9195 Grant Street, Suite 305 Thornton, CO 80229<br />
+      Phone: (720) 678-9868 | Fax: (720) 678-9860
     </p>
 
-    <p>
+    <!-- <p>
       <big><strong>Aurora, CO</strong></big><br />
       1421 S. Potomac Street, Suite 330<br />
       Aurora, CO 80012
-    </p>
+    </p> -->
   </div>
-  <p>Phone: (720) 678-9868 | Fax: (720) 678-9860</p>
+  <!-- <p>Phone: (720) 678-9868 | Fax: (720) 678-9860</p> -->
 </Section>
 
 <style>
@@ -35,6 +35,7 @@
     color: white;
   }
   h4 {
+    padding-inline: 2rem;
     color: gold;
     text-align: center;
   }
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,9 +14,7 @@
   />
   <title>Restore Medical Group | Golden, CO</title>
 
-  <script
-    async
-    src="https://www.googletagmanager.com/gtag/js?id=UA-185413763-1"></script>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-185413763-1"></script>
   <script>
     window.dataLayer = window.dataLayer || []
     function gtag() {
@@ -44,20 +42,17 @@
         <p>
           Welcome to the
           <strong>Restore Medical Group</strong>
-          Wound Care division - a truly unique health care practice. Restore Medical
-          Group has developed an insurance backed protocol for treating recalcitrant
-          wounds that prove difficult to heal using conventional wound care methods.
+          Wound Care division - a truly unique health care practice. Restore Medical Group has developed
+          an insurance backed protocol for treating recalcitrant wounds that prove difficult to heal
+          using conventional wound care methods.
         </p>
 
         <p>
-          Our treatment protocol incorporates a combination of therapies
-          including our exclusively sourced amnio products that rapidly promote
-          healing in wound areas.
+          Our treatment protocol incorporates a combination of therapies including our exclusively
+          sourced amnio products that rapidly promote healing in wound areas.
         </p>
 
-        <p>
-          Our approach can provide either in-clinic or on-location care options.
-        </p>
+        <p>Our approach can provide either in-clinic or on-location care options.</p>
 
         <p>
           <strong>Most insurance accepted, including Medicare Part B</strong>
@@ -82,8 +77,8 @@
     <Grid columns="1fr 1fr" gap="2em">
       <div>
         <p>
-          When evaluating patients for this protocol please consider the
-          following before recommending care.
+          When evaluating patients for this protocol please consider the following before
+          recommending care.
         </p>
         <figure>
           <img
@@ -93,10 +88,7 @@
         </figure>
       </div>
       <article>
-        <p>
-          When evaluating patients referral please consider the following
-          contraindications:
-        </p>
+        <p>When evaluating patients referral please consider the following contraindications:</p>
         <ul>
           <li>infected wound,</li>
           <li>active cancer treatment,</li>
@@ -105,16 +97,17 @@
 
         <h5>Pre-Assessment</h5>
         <p>
-          Restore Medical will conduct assessment and amnio products
-          preauthorization on all patients prior to treatment to ensure the
-          protocol is applicable and insurance is confirmed for the treatment.
+          Restore Medical will conduct assessment and amnio products preauthorization on all
+          patients prior to treatment to ensure the protocol is applicable and insurance is
+          confirmed for the treatment.
         </p>
 
-        <h5>Transport</h5>
+        <h5>Location</h5>
         <p>
-          Transport to our clinical locations can be arranged on behalf of the
-          patient if not provided with their current facility or home care
-          provider.
+          No need to drive, we will come to you! Our flexible mobile service is available if
+          transportation and mobility is a problem. We bill your insurance for the visit so there is
+          no cash fee involved. Additionally, we have multiple clinic locations available for
+          evaluations and treatments if you'd prefer.
         </p>
       </article>
     </Grid>
@@ -132,22 +125,13 @@
         <cite>-Maxine (Patient)</cite>
       </blockquote>
       <p>Check out the testimonials from previous patients.</p>
-      <Button
-        background="dodgerblue"
-        url="/testimonials"
-        color="white"
-        rounded
-        shadow
-        uppercase>Watch The Videos</Button
+      <Button background="dodgerblue" url="/testimonials" color="white" rounded shadow uppercase
+        >Watch The Videos</Button
       >
     </div>
   </Section>
 
-  <Section
-    boxed
-    background="url(images/snow.jpg) center center/cover"
-    height="500px"
-  >
+  <Section boxed background="url(images/snow.jpg) center center/cover" height="500px">
     <Parallax speed="0.1">
       <Flex align="start" gap="2em">
         <CTA
